7a7972a75ffe584069fd2f649253f506eed72bbd
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-12-15 16:37:17 +0800

    Project preparation

---
+++ develop.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+# Script to control ${PROJECT_NAME} in dev and test
+
+TOPDIR=$(cd $(dirname $0); pwd)
+
+ACTION=$1
+
+PROJECT_NAME='bpatrack'
+VIRTUALENV="${HOME}/virt_${PROJECT_NAME}"
+: ${DOCKER_BUILD_OPTIONS:="--pull=true"}
+
+DATE=$(date +%Y.%m.%d)
+
+######### Logging ##########
+COLOR_NORMAL=$(tput sgr0)
+COLOR_RED=$(tput setaf 1)
+COLOR_YELLOW=$(tput setaf 3)
+COLOR_GREEN=$(tput setaf 2)
+
+set -e
+
+log_error() {
+    echo ${COLOR_RED}ERROR: $* ${COLOR_NORMAL}
+}
+
+log_warning() {
+    echo ${COLOR_YELLOW}WARNING: $* ${COLOR_NORMAL}
+}
+
+log_success() {
+    echo ${COLOR_GREEN}SUCCESS: $* ${COLOR_NORMAL}
+}
+
+log_info() {
+    echo INFO: $*
+}
+
+# no news is good news
+log() {
+    ERROR_CODE=$0
+    MESSAGE=$1
+
+    if [ ${ERROR_CODE} != 0 ]
+    then
+        log_warning ${MESSAGE}
+    else
+        log_success ${MESSAGE}
+    fi
+ }
+
+activate_virtualenv() {
+   if [ ! -d ${VIRTUALENV} ]
+   then
+        log_warning "There is no ${VIRTUALENV} here, making it."
+        virtualenv ${VIRTUALENV}
+        . ${VIRTUALENV}/bin/activate
+        # there's little point in ensuring docker-compose is available if docker 
+        # isn't
+        # pip install docker-compose
+        pip install 'flake8>=2.0,<2.1'
+   else
+      source ${VIRTUALENV}/bin/activate
+   fi
+}
+
+pythonlint() {
+    activate_virtualenv
+    # find . -type d -name ".ropeproject" -exec rm -fr {} \;
+    ${VIRTUALENV}/bin/flake8 bpam --exclude=migrations,.ropeproject --ignore=E501,E303 --count
+}
+
+
+unit_tests() {
+    activate_virtualenv
+
+    mkdir -p data/tests
+    chmod o+rwx data/tests
+    docker-compose --project-name ${PROJECT_NAME} -f fig-test.yml up
+}
+
+up() {
+    activate_virtualenv
+    mkdir -p data/dev
+    chmod o+rwx data/dev
+
+    docker-compose --project-name ${PROJECT_NAME} up
+}
+
+
+selenium() {
+    activate_virtualenv
+    mkdir -p data/selenium
+    chmod o+rwx data/selenium
+
+    docker-compose --project-name ${PROJECT_NAME} -f fig-selenium.yml up
+}
+
+
+build() {
+   activate_virtualenv
+   docker-compose --project-name ${PROJECT_NAME} build
+}
+
+rm_containers() {
+   docker-compose --project-name ${PROJECT_NAME} rm
+}
+
+entrypoint() {
+   ENTRYPOINT=${1:-bash}
+   log_info "Entrypoint ${ENTRYPOINT}"
+   docker exec -it ${PROJECT_NAME}_runserver_1 ${ENTRYPOINT} $2
+}
+
+
+dockerbuild() {
+   activate_virtualenv
+
+   image="muccg/${PROJECT_NAME}"
+   gitbranch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+   gittag=$(git describe --abbrev=0 --tags 2> /dev/null)
+
+   # only use tags when on master (release) branch
+   if [ $gitbranch != "master" ]
+   then
+      echo "Ignoring tags, not on master branch"
+      gittag=$gitbranch
+   fi
+
+   # if no git tag, then use branch name
+   if [ -z ${gittag+x} ]
+   then
+      echo "No git tag set, using branch name"
+      gittag=$gitbranch
+   fi
+
+   echo "############################################################# ${PROJECT_NAME} ${gittag}"
+
+   # attempt to warm up docker cache
+   docker pull ${image} || true
+
+   for tag in "${image}:${gittag}" "${image}:${gittag}-${DATE}"
+   do
+      echo "############################################################# ${PROJECT_NAME} ${tag}"
+      set -x
+      docker build ${DOCKER_BUILD_OPTIONS} --build-arg GIT_TAG=${gittag} -t ${tag} -f Dockerfile.release .
+      docker push ${tag}
+      set +x
+   done
+}
+
+
+usage() {
+   echo 'Usage ./develop.sh (build|shell|unit_tests|selenium|superuser|up|rm|runscript|ingest_all)'
+
+   echo '                   build        Build all images'
+   echo '                   dockerbuild  Build and push new docker images from current checked out tag'
+   echo '                   shell        Create and shell into a new web image, used for db checking with Django env available'
+   echo '                   superuser    Create Django superuser'
+   echo '                   runscript    Run one of the available scripts' 
+   echo '                   ingest_all   Ingest metadata'
+   echo '                   checksecure  Run security check'
+   echo '                   up           Spins up docker development stack'
+   echo '                   rm           Remove all containers'
+   echo '                   pythonlint   Run python lint'
+   echo '                   unit_tests   Run unit tests'
+   echo '                   selenium     Run selenium tests'
+   echo '                   usage'
+}
+
+case ${ACTION} in
+pythonlint)
+    pythonlint
+    ;;
+ci_staging)
+    ci_ssh_agent
+    ci_staging
+    ;;
+start)
+    up
+    ;;
+build)
+    build
+    ;;
+dockerbuild)
+    dockerbuild
+    ;;
+rm)
+    rm_containers
+    ;;
+clean)
+    clean
+    ;;
+up)
+    up
+    ;;
+shell)
+    docker exec -it ${PROJECT_NAME}_runserver_1 /bin/bash
+    ;;
+admin)
+    docker exec -it ${PROJECT_NAME}_runserver_1 /app/docker-entrypoint.sh admin $2
+    ;;
+superuser)
+    docker exec -it ${PROJECT_NAME}_runserver_1 /app/docker-entrypoint.sh superuser
+    ;;
+runscript)
+    docker exec -it ${PROJECT_NAME}_runserver_1 /app/docker-entrypoint.sh runscript $2
+    ;;
+nuclear)
+    docker exec -it ${PROJECT_NAME}_runserver_1 /app/docker-entrypoint.sh nuclear
+    ;;
+unit_tests)
+    unit_tests
+    ;;
+selenium)
+    selenium
+    ;;
+checksecure)
+    docker exec -it ${PROJECT_NAME}_runserver_1 /app/docker-entrypoint.sh checksecure
+    ;;
+*)
+    usage
+esac
---
+++ docker-entrypoint.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+COMMAND=$1
+
+# wait for a given host:port to become available
+dockerwait() {
+    host=$1
+    port=$2
+    while ! exec 6<>/dev/tcp/${host}/${port}
+    do
+        echo "$(date) - waiting to connect ${host} ${port}"
+        sleep 5
+    done
+    echo "$(date) - connected to $host $port"
+
+    exec 6>&-
+    exec 6<&-
+}
+
+
+# wait for services to become available
+# this prevents race conditions using fig
+wait_for_services() {
+    if [[ "$WAIT_FOR_QUEUE" ]] ; then
+        dockerwait $QUEUESERVER $QUEUEPORT
+    fi
+    if [[ "$WAIT_FOR_DB" ]] ; then
+        dockerwait $DBSERVER $DBPORT
+    fi
+    if [[ "$WAIT_FOR_CACHE" ]] ; then
+        dockerwait $CACHESERVER $CACHEPORT
+    fi
+    if [[ "$WAIT_FOR_WEB" ]] ; then
+        dockerwait $WEBSERVER $WEBPORT
+    fi
+}
+
+
+defaults() {
+
+    : ${ENV_PATH:="/env/bin"}
+
+    : ${DBSERVER:="db"}
+    : ${DBPORT:="5432"}
+    : ${WEBSERVER="web"}
+    : ${WEBPORT="8000"}
+    : ${CACHESERVER="cache"}
+    : ${CACHEPORT="11211"}
+
+    : ${DBUSER="webapp"}
+    : ${DBNAME="${DBUSER}"}
+    : ${DBPASS="${DBUSER}"}
+
+    . ${ENV_PATH}/activate
+
+    export DBSERVER DBPORT DBUSER DBNAME DBPASS
+}
+
+
+django_defaults() {
+    : ${DEPLOYMENT="dev"}
+    : ${PRODUCTION=0}
+    : ${DEBUG=1}
+    : ${MEMCACHE="${CACHESERVER}:${CACHEPORT}"}
+    : ${WRITABLE_DIRECTORY="/data/scratch"}
+    : ${STATIC_ROOT="/data/static"}
+    : ${MEDIA_ROOT="/data/static/media"}
+    : ${LOG_DIRECTORY="/data/log"}
+    : ${DJANGO_SETTINGS_MODULE="datatracker.settings"}
+
+    echo "DEPLOYMENT is ${DEPLOYMENT}"
+    echo "PRODUCTION is ${PRODUCTION}"
+    echo "DEBUG is ${DEBUG}"
+    echo "MEMCACHE is ${MEMCACHE}"
+    echo "WRITABLE_DIRECTORY is ${WRITABLE_DIRECTORY}"
+    echo "STATIC_ROOT is ${STATIC_ROOT}"
+    echo "MEDIA_ROOT is ${MEDIA_ROOT}"
+    echo "LOG_DIRECTORY is ${LOG_DIRECTORY}"
+    echo "DJANGO_SETTINGS_MODULE is ${DJANGO_SETTINGS_MODULE}"
+    export DEPLOYMENT PRODUCTION DEBUG DBSERVER MEMCACHE WRITABLE_DIRECTORY STATIC_ROOT MEDIA_ROOT LOG_DIRECTORY DJANGO_SETTINGS_MODULE
+}
+
+echo "HOME is ${HOME}"
+echo "WHOAMI is $(whoami)"
+
+defaults
+django_defaults
+wait_for_services
+
+if [ "${COMMAND}" = 'nuclear' ]
+then
+    django-admin.py reset_db --router=default --traceback --settings=${DJANGO_SETTINGS_MODULE}
+    django-admin.py migrate --traceback --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/ingest.log
+    exit $?
+fi
+
+if [ "${COMMAND}" = 'runscript' ]
+then
+    echo "Runscript $2"
+    django-admin.py runscript $2 --traceback --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/runscript.log
+    exit $?
+fi
+
+if [ "${COMMAND}" = 'admin' ]
+then
+    echo "admin"
+    if [ "$2" = "" ]
+    then 
+        admincmd="help"
+    else
+        admincmd=$2
+    fi
+    django-admin.py $admincmd --settings=${DJANGO_SETTINGS_MODULE}
+    exit $?
+fi
+
+# set superuser 
+if [ "${COMMAND}" = 'superuser' ]
+then
+    echo "Setting superuser (admin)"
+    django-admin.py  createsuperuser --email="admin@ccg.com" --settings=${DJANGO_SETTINGS_MODULE}
+    exit $?
+fi
+
+# security by django checksecure
+if [ "$COMMAND" = 'checksecure' ]
+then
+    echo "[Run] Running Django checksecure"
+    django-admin.py checksecure --traceback --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/checksecure.log
+
+    exit $?
+fi
+
+# uwsgi entrypoint
+if [ "$COMMAND" = 'uwsgi' ]
+then
+    echo "[Run] Starting uwsgi"
+
+    : ${UWSGI_OPTS="/app/uwsgi/docker.ini"}
+    echo "UWSGI_OPTS is ${UWSGI_OPTS}"
+
+    django-admin.py collectstatic --noinput --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/uwsgi-collectstatic.log
+    django-admin.py syncdb --noinput --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/uwsgi-syncdb.log
+    django-admin.py migrate --noinput --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/uwsgi-migrate.log
+    exec uwsgi --die-on-term --ini ${UWSGI_OPTS}
+fi
+
+# runserver entrypoint
+if [ "$COMMAND" = 'runserver' ]
+then
+    echo "[Run] Starting runserver"
+
+    : ${RUNSERVER_OPTS="runserver_plus 0.0.0.0:${WEBPORT} --settings=${DJANGO_SETTINGS_MODULE}"}
+    echo "RUNSERVER_OPTS is ${RUNSERVER_OPTS}"
+
+    echo "Django collectstatic"
+    django-admin.py collectstatic --noinput --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/runserver-collectstatic.log
+    echo "Django migrate"
+    django-admin.py migrate --noinput --settings=${DJANGO_SETTINGS_MODULE} 2>&1 | tee /data/runserver-migrate.log
+    
+    echo "Django runserver"
+    exec django-admin.py ${RUNSERVER_OPTS}
+fi
+
+# runtests entrypoint
+if [ "$COMMAND" = 'runtests' ] 
+then
+    echo "Django test"
+    cd /app/datatracker
+    exec django-admin.py test --traceback --settings=${DJANGO_SETTINGS_MODULE}
+fi
+
+# lettuce entrypoint
+if [ "$COMMAND" = 'lettuce' ]
+then
+    echo "[Run] Starting lettuce"
+    exec django-admin.py run_lettuce --with-xunit --xunit-file=/data/tests.xml
+fi
+
+echo "[RUN]: Builtin command not provided [lettuce|runtests|runserver|uwsgi|checksecure|superuser|nuclear|ingest|runscript]"
+echo "[RUN]: $@"
+
+exec "$@"
