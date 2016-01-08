5fe90eb9198c9585378235d43264cec6e5eaa060
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-01-08 13:49:26 +0800

    Adds base

--- config/settings/common.py
+++ config/settings/common.py
@@ -46,7 +46,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'bpa_datatracker.users',  # custom users app
-    # Your stuff: custom apps go here
+    'bpa_datatracker.base',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
