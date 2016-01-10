77d10ee40a99022d089dbbc1af6670956ddecaf4
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-01-10 23:16:31 +0800

    Userwidget to create users when encountered

--- bpa_datatracker/base/admin.py
+++ bpa_datatracker/base/admin.py
@@ -1,32 +1,92 @@
 from dateutil.parser import parse as date_parser
 from django.contrib import admin
-from import_export import resources, fields
+from import_export import resources, fields, widgets
 from import_export.admin import ImportExportModelAdmin
 
-from .models import SampleReception
+from bpa_datatracker.users.models import User
+from .models import SampleReceived
 
 class DateField(fields.Field):
+    """
+    This field automatically parses a number of known date formats and returns
+    the standard python date
+    """
+
     def __init__(self, *args, **kwargs):
         super(DateField, self).__init__(*args, **kwargs)
 
     def clean(self, data):
         return date_parser(data[self.column_name])
 
-class SampleReceptionResource(resources.ModelResource):
+class UserWidget(widgets.ForeignKeyWidget):
+    def __init__(self):
+        self.model = User
+        
+        self.username = ""
+        self.firstname = ""
+        self.lastname = ""
+
+    def _set_name(self, name_from_source):
+        "use first letter from first name and whole of last name, eurocentric"
+
+        parts = name_from_source.split()
+        if len(parts) >= 2:
+            self.firstname = parts[0]
+            self.lastname = parts[-1]
+            self.username = self.firstname[0] + self.lastname
+        else:
+            self.firstname = parts[0]
+            self.lastname = parts[0]
+            self.username = self.lastname
+
+
+    def clean(self, value):
+        self._set_name(value)
+
+        user, _ = self.model.objects.get_or_create(
+                first_name=self.firstname,
+                last_name=self.lastname,
+                username=self.username)
+
+        return user
+
+    def render(self, value):
+        return value
+
+
+class SampleReceivedResource(resources.ModelResource):
 
     vendor = fields.Field(attribute='vendor', column_name='Vendor')
     extraction_id = fields.Field(attribute='extraction_id', column_name='Sample extraction ID')
     batch_number = fields.Field(attribute='batch_number', column_name='Batch number')
-    submitter = fields.Field(attribute='submitter__name', column_name='Submitter Name')
-    # date_received = fields.Field(attribute='date_received', column_name='Date received')
     date_received = DateField(attribute='date_received', column_name='Date received')
 
+    submitter = fields.Field(
+            attribute='submitter',
+            column_name='Submitter Name',
+            # widget=widgets.ForeignKeyWidget(User, field="username"))
+            widget=UserWidget()
+            )
+
     class Meta:
-        model = SampleReception
-        import_id_fields = ('extraction_id',)
+        model = SampleReceived
+        import_id_fields = ('extraction_id', )
+        export_order = (
+                'extraction_id',
+                'vendor',
+                'batch_number',
+                'date_received',
+                'submitter')
 
+@admin.register(SampleReceived)
+class SampleReceivedAdmin(ImportExportModelAdmin):
+    resource_class = SampleReceivedResource
+    list_display = (
+            'vendor',
+            'extraction_id',
+            'batch_number',
+            'date_received',
+            'submitter')
+    date_hierarchy = 'date_received'
+    empty_value_display = '-empty-'
 
-class SampleReceptionAdmin(ImportExportModelAdmin):
-    resource_class = SampleReceptionResource
-
-admin.site.register(SampleReception, SampleReceptionAdmin)
--- bpa_datatracker/base/models.py
+++ bpa_datatracker/base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from bpa_datatracker.users.models import User
 
-class SampleReception(models.Model):
+class SampleReceived(models.Model):
     """ Notes the reception of a sample at a Vendor """
 
     VENDORS = (
@@ -13,7 +13,11 @@ class SampleReception(models.Model):
     extraction_id = models.CharField(max_length=12, primary_key=True)
     batch_number = models.CharField(max_length=20)
     date_received = models.DateField()
-    submitter = User()
+    submitter = models.ForeignKey(User, null=True)
 
     def __str__(self):
         return "{} {} received at {}".format(self.extraction_id, self.batch_number, self.date_received)
+
+    class Meta:
+        verbose_name = 'Sample Received'
+        verbose_name_plural = 'Samples Received'
