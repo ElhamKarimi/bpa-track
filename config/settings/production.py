085a2bc2bce4621bc32375644b64d2c44863ffc2
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-02-10 14:33:57 +0800

    Allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -50,7 +50,7 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['ccgapps.com.au'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['staging.ccgapps.com.au', 'ccgapps.com.au'])
 # END SITE CONFIGURATION
 
 
