fb11e8e184c8c5a88462d12624dcfafae49c3bb1
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-02-11 08:24:53 +0800

    wsgi env

--- bpatrack/config/wsgi.py
+++ bpatrack/config/wsgi.py
@@ -29,7 +29,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bpatrack.config.settings.produc
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-application = get_wsgi_application()
+# application = get_wsgi_application()
 
 # Use Whitenoise to serve static files
 # See: https://whitenoise.readthedocs.org/
@@ -39,3 +39,11 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+def application(environ, start):
+
+    # copy any vars into os.environ
+    for key in environ:
+        os.environ[key] = str(environ[key])
+
+    return get_wsgi_application()(environ, start)
