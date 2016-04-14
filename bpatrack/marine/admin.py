9e355faea5e46e418f212ff3f3a84a608a327840
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-04-14 14:20:30 +0800

    Import/Export for Marine Open Water

--- bpatrack/marine/admin.py
+++ bpatrack/marine/admin.py
@@ -161,7 +161,6 @@ class ContextualPelagicResource(CommonWaterResource):
     class Meta:
         model = ContextualPelagic
         import_id_fields = ('bpa_id', )
-        # exclude = ('site', )
         export_order = ('bpa_id',
                         'sample_site_name',
                         'lat',
@@ -257,15 +256,126 @@ class ContextualOpenWaterResource(CommonWaterResource):
     conductivity = fields.Field(attribute="conductivity", column_name="Conductivity [S/m]")
     turbitity = fields.Field(attribute="turbitity", column_name="Turbidity (Upoly 0, WET Labs FLNTURT)")
     salinity = fields.Field(attribute="salinity", column_name="Salinity [PSU] Laboratory")
+    salinity_cdt = fields.Field(attribute="salinity_cdt", column_name="Salinity [PSU] CTD")
     microbial_abandance = fields.Field(attribute="microbial_abundance", column_name="Microbial abundance (cells per ml)")
     chlorophyl = fields.Field(attribute="chlorophyl", column_name="Chlorophyll a (μg/L)")
     carbon_total = fields.Field(attribute="carbon_total", column_name="% total carbon")
     inorganic_carbon_total = fields.Field(attribute="inorganic_carbon_total", column_name="% total inorganc carbon")
     flux = fields.Field(attribute="flux", column_name="Light intensity (lux)")
+    silicate = fields.Field(attribute="silicate", column_name="Silicate (μmol/L)")
+    fluorescence = fields.Field(attribute="fluorescence", column_name="Fluorescence, Wetlab ECO-AFL/FL [mg/m^3]")
+    density = fields.Field(attribute="density", column_name="Density [density, Kg/m^3]")
+    tss = fields.Field(attribute="tss", column_name="TSS [mg/L]")
+    inorganic_fraction = fields.Field(attribute="inorganic_fraction", column_name="Inorganic Fraction [mg/L]")
+    organic_fraction = fields.Field(attribute="organic_fraction", column_name="Organic Fraction [mg/L]")
+    secchi_depth = fields.Field(column_name="Secchi Depth (m)")
+    biomass = fields.Field(attribute="biomass", column_name="Biomass (mg/m3)")
+    allo = fields.Field(attribute="allo", column_name="ALLO [mg/m3]")
+    alpha_beta_car = fields.Field(attribute="alpha_beta_car", column_name="ALPHA_BETA_CAR [mg/m3]")
+    nth = fields.Field(attribute="nth", column_name="NTH [mg/m3]")
+    asta = fields.Field(attribute="asta", column_name="ASTA [mg/m3]")
+    beta_beta_car = fields.Field(attribute="beta_beta_car", column_name="BETA_BETA_CAR [mg/m3]")
+    beta_epi_car = fields.Field(attribute="beta_epi_car", column_name="BETA_EPI_CAR [mg/m3]")
+    but_fuco = fields.Field(attribute="but_fuco", column_name="BUT_FUCO [mg/m3]")
+    cantha = fields.Field(attribute="cantha", column_name="CANTHA [mg/m3]")
+    cphl_a = fields.Field(attribute="cphl_a", column_name="CPHL_A [mg/m3] ")
+    cphl_b = fields.Field(attribute="cphl_b", column_name="CPHL_B [mg/m3]")
+    cphl_c1c2 = fields.Field(attribute="cphl_c1c2", column_name="CPHL_C1C2 [mg/m3]")
+    cphl_c1 = fields.Field(attribute="cphl_c1", column_name="CPHL_C1 [mg/m3]")
+    cphl_c2 = fields.Field(attribute="cphl_c2", column_name="CPHL_C2 [mg/m3]")
+    cphl_c3 = fields.Field(attribute="cphl_c3", column_name="CPHL_C3 [mg/m3]")
+    cphlide_a = fields.Field(attribute="cphlide_a", column_name="CPHLIDE_A [mg/m3]")
+    diadchr = fields.Field(attribute="diadchr", column_name="DIADCHR [mg/m3]")
+    diadino = fields.Field(attribute="diadino", column_name="DIADINO [mg/m3]")
+    diato = fields.Field(attribute="diato", column_name="DIATO [mg/m3]")
+    dino = fields.Field(attribute="dino", column_name="DINO [mg/m3]")
+    dv_cphl_a_and_cphl_a = fields.Field(attribute="dv_cphl_a_and_cphl_a", column_name="DV_CPHL_A_and_CPHL_A [mg/m3]")
+    dv_cphl_a = fields.Field(attribute="dv_cphl_a", column_name="DV_CPHL_A [mg/m3]")
+    dv_cphl_b_and_cphl_b = fields.Field(attribute="dv_cphl_b_and_cphl_b", column_name="DV_CPHL_B_and_CPHL_B [mg/m3]")
+    dv_cphl_b = fields.Field(attribute="dv_cphl_b", column_name="DV_CPHL_B [mg/m3]")
+    echin = fields.Field(attribute="echin", column_name="ECHIN [mg/m3]")
+    fuco = fields.Field(attribute="fuco", column_name="FUCO [mg/m3]")
+    gyro = fields.Field(attribute="gyro", column_name="GYRO [mg/m3]")
+    hex_fuco = fields.Field(attribute="hex_fuco", column_name="HEX_FUCO [mg/m3]")
+    keto_hex_fuco = fields.Field(attribute="keto_hex_fuco", column_name="KETO_HEX_FUCO [mg/m3]")
+    lut = fields.Field(attribute="lut", column_name="LUT [mg/m3]")
+    lyco = fields.Field(attribute="lyco", column_name="LYCO [mg/m3]")
+    mg_dvp = fields.Field(attribute="mg_dvp", column_name="MG_DVP [mg/m3]")
+    neo = fields.Field(attribute="neo", column_name="NEO [mg/m3]")
+    perid = fields.Field(attribute="perid", column_name="PERID [mg/m3]")
+    phide_a = fields.Field(attribute="phide_a", column_name="PHIDE_A [mg/m3]")
+    phytin_a = fields.Field(attribute="phytin_a", column_name="PHYTIN_A [mg/m3]")
+    phytin_b = fields.Field(attribute="phytin_b", column_name="PHYTIN_B [mg/m3]")
+    pras = fields.Field(attribute="pras", column_name="PRAS [mg/m3] ")
+    pyrophide_a = fields.Field(attribute="pyrophide_a", column_name="PYROPHIDE_A [mg/m3]")
+    pyrophytin_a = fields.Field(attribute="pyrophytin_a", column_name="PYROPHYTIN_A [mg/m3]")
+    viola = fields.Field(attribute="viola", column_name="VIOLA [mg/m3]")
+    zea = fields.Field(attribute="zea", column_name="ZEA [mg/m3]")
 
     class Meta:
         model = ContextualOpenWater
         import_id_fields = ('bpa_id', )
+
+        export_order = ('bpa_id',
+                        'sample_site_name',
+                        'lat',
+                        'lon',
+                        'depth',
+                        'date_sampled',
+                        'time_sampled',
+                        'note',
+                        'host_species',
+                        'ph',
+                        'oxygen',
+                        'oxygen_ctd',
+                        'silicate',
+                        'nitrate',
+                        'fluorescence',
+                        'tss',
+                        'inorganic_fraction',
+                        'biomass',
+                        'allo',
+                        'alpha_beta_car',
+                        'nth',
+                        'asta',
+                        'beta_beta_car',
+                        'beta_epi_car',
+                        'but_fuco',
+                        'cantha',
+                        'cphl_a',
+                        'cphl_b',
+                        'cphl_c1c2',
+                        'cphl_c1',
+                        'cphl_c2',
+                        'cphl_c3',
+                        'cphlide_a',
+                        'diadchr',
+                        'diadino',
+                        'diato',
+                        'dino',
+                        'dv_cphl_a_and_cphl_a',
+                        'dv_cphl_a',
+                        'dv_cphl_b_and_cphl_b',
+                        'dv_cphl_b',
+                        'echin',
+                        'fuco',
+                        'gyro',
+                        'hex_fuco',
+                        'keto_hex_fuco',
+                        'lut',
+                        'lyco',
+                        'mg_dvp',
+                        'neo',
+                        'perid',
+                        'phide_a',
+                        'phytin_a',
+                        'phytin_b',
+                        'pras',
+                        'pyrophide_a',
+                        'pyrophytin_a',
+                        'viola',
+                        'zea',
+                        )
 
 
 class ContextualOpenWaterAdmin(ImportExportModelAdmin):
@@ -275,6 +385,7 @@ class ContextualOpenWaterAdmin(ImportExportModelAdmin):
             'bpa_id',
             'date_sampled',
             'site',
+            'depth',
             )
 
     _required = (
